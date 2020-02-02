1141af404a922e5059f968e02f70f9181c7fd94a
Vishal Lakhani <vishal4projects@outlook.com>
2020-02-02 13:12:48 +0530

    helloworld progress

--- app/src/main/java/com/vlxstudios/kotlinbasicss/helloworld.kt
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/helloworld.kt
@@ -1,18 +1,81 @@
 package com.vlxstudios.kotlinbasicss
 
-fun main()    {
-    println("Hello world!")
-    sum(5,7)
-    println("sum " +sum(5,7))
-    sum2(5,6,7)
-}
+val i:Int=0
+    fun main() {
+        println("Hello world!") // basic println
+       // sum(5, 7)
+        println("sum " + sum(5, 7)) // this is return function
+        sum2(5, 6, 7) //this is void function
+        println("max " + maxOf(5, 7)) // this is expression conditions
+        printProductofnumer("5","7")
+
+        println("String Length: " +  CheckStringLen("Vishal") )
+        loopCheck() // for loop or foreach
+        whileCheck() // while loop
+        println("Check Object " + DescribeObjTrype("")) //switch statement
+
+    }
 //fun refers to function
 //fun main() is the entry point
 
-fun sum(a: Int, b: Int) = a + b  // getting values and immediate returning it with answer.
+    fun sum(a: Int, b: Int) = a + b  // getting values and immediate returning it with answer.
 
-fun sum2(a:Int,b:Int,c:Int) : Unit{   // unit is something which doesnt returns any value just like void in C# else if dont write "unit" it will still consider void.
+    fun sum2(
+        a: Int,
+        b: Int,
+        c: Int
+    ): Unit {   // unit is something which doesnt returns any value just like void in C# else if dont write "unit" it will still consider void.
 
-    println("Sum2 is ${a+b+c}")
-    println("Sum3 is ${a+b+c}")
+        println("Sum2 is ${a + b + c}")
+
+    }
+
+    fun maxOf(a: Int, b: Int) = if (a > b) a else b // expression conditions
+
+fun  parseInt(str:String) = if(str.toInt() == str.toInt())str.toInt() else if (str.toInt() !is Int) null else null
+
+
+
+fun printProductofnumer(arg1:String,arg2:String){
+    var x = parseInt(arg1)
+    var y = parseInt(arg2)
+
+    if(x != null && y != null)
+    {
+        println("Product is "+ x*y);
+    }else   {
+        println("$x or $y is not a number")
+    }
+
+
 }
+fun CheckStringLen(str:String): Int ?{
+    if(str is String){
+        return str.length
+    }
+    return null
+}
+
+fun loopCheck(){
+    val items = listOf<String>("Apple","Banana","Kiwi")
+    for(item in items){
+        println("for Loop items "+item)
+    }
+}
+fun whileCheck(){
+    val items = listOf<String>("Apple","Banana","Kiwi")
+    var index = 0
+    while(index < items.size){
+        println("while loop items " + items[index])
+        index++
+    }
+}
+
+fun DescribeObjTrype(obj:Any):String =
+    when(obj){
+        1 -> "one"
+        "Hello" -> "Greeting"
+        is Long -> "Long"
+        !is String -> "Not a String"
+        else -> "Unknown Object type"
+    }
