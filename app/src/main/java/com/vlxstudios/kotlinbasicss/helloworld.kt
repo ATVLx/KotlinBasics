64af81a4c49ab9010cd8cbeff001cae60a315c20
Vishal Lakhani <vishal4projects@outlook.com>
2020-02-03 10:35:43 +0530

    base

--- app/src/main/java/com/vlxstudios/kotlinbasicss/helloworld.kt
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/helloworld.kt
@@ -1,20 +1,24 @@
 package com.vlxstudios.kotlinbasicss
 
-val i:Int=0
+    val i: Int = 0
     fun main() {
         println("Hello world!") // basic println
-       // sum(5, 7)
+        // sum(5, 7)
         println("sum " + sum(5, 7)) // this is return function
         sum2(5, 6, 7) //this is void function
         println("max " + maxOf(5, 7)) // this is expression conditions
-        printProductofnumer("5","7")
+        printProductofnumer("5", "7")
 
-        println("String Length: " +  CheckStringLen("Vishal") )
+        println("String Length: " + CheckStringLen("Vishal"))
         loopCheck() // for loop or foreach
         whileCheck() // while loop
         println("Check Object " + DescribeObjTrype("")) //switch statement
+        println("Range check (5,10,7)" + CheckRange(5, 7, 6))
 
-    }
+
+
+        }
+
 //fun refers to function
 //fun main() is the entry point
 
@@ -32,50 +36,59 @@ val i:Int=0
 
     fun maxOf(a: Int, b: Int) = if (a > b) a else b // expression conditions
 
-fun  parseInt(str:String) = if(str.toInt() == str.toInt())str.toInt() else if (str.toInt() !is Int) null else null
+    fun parseInt(str: String) = if (str.toInt() == str.toInt()) str.toInt() else if (str.toInt() !is Int) null else null
 
 
+    fun printProductofnumer(arg1: String, arg2: String) {
+        var x = parseInt(arg1)
+        var y = parseInt(arg2)
 
-fun printProductofnumer(arg1:String,arg2:String){
-    var x = parseInt(arg1)
-    var y = parseInt(arg2)
+        if (x != null && y != null) {
+            println("Product is " + x * y);
+        } else {
+            println("$x or $y is not a number")
+        }
 
-    if(x != null && y != null)
-    {
-        println("Product is "+ x*y);
-    }else   {
-        println("$x or $y is not a number")
+
     }
 
+    fun CheckStringLen(str: String): Int? {
+        if (str is String) {
+            return str.length
+        }
+        return null
+    }
 
-}
-fun CheckStringLen(str:String): Int ?{
-    if(str is String){
-        return str.length
+    fun loopCheck() {
+        val items = listOf<String>("Apple", "Banana", "Kiwi")
+        for (item in items) {
+            println("for Loop items " + item)
+        }
     }
-    return null
-}
 
-fun loopCheck(){
-    val items = listOf<String>("Apple","Banana","Kiwi")
-    for(item in items){
-        println("for Loop items "+item)
+    fun whileCheck() {
+        val items = listOf<String>("Apple", "Banana", "Kiwi")
+        var index = 0
+        while (index < items.size) {
+            println("while loop items " + items[index])
+            index++
+        }
     }
-}
-fun whileCheck(){
-    val items = listOf<String>("Apple","Banana","Kiwi")
-    var index = 0
-    while(index < items.size){
-        println("while loop items " + items[index])
-        index++
-    }
-}
 
-fun DescribeObjTrype(obj:Any):String =
-    when(obj){
-        1 -> "one"
-        "Hello" -> "Greeting"
-        is Long -> "Long"
-        !is String -> "Not a String"
-        else -> "Unknown Object type"
+    fun DescribeObjTrype(obj: Any): String =
+        when (obj) {
+            1 -> "one"
+            "Hello" -> "Greeting"
+            is Long -> "Long"
+            !is String -> "Not a String"
+            else -> "Unknown Object type"
+        }
+
+    fun CheckRange(min: Int, max: Int, value: Int): String {
+        if (value in min..max) {
+            return "fits in range"
+        } else {
+            return "doesn't Fits in range"
+        }
     }
+
---
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/main.kt
@@ -0,0 +1,10 @@
+package com.vlxstudios.kotlinbasicss
+
+class vlx {
+public  fun add(a:Int,b:Int):Int{
+    return a+b;
+}
+}
+fun main(args: Array<String>){
+    println("Hello kotlin")
+}
