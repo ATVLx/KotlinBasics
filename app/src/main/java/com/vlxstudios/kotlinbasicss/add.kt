9ea3f828e5478ef138a64356a2a3a6693162f435
Vishal <vlxmenblack@gmail.com>
2020-02-04 17:38:27 +0530

    commit changes

--- app/src/main/java/com/vlxstudios/kotlinbasicss/add.kt
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/add.kt
@@ -1,6 +1,67 @@
 package com.vlxstudios.kotlinbasicss
 
 
+fun sum(a: Int, b: Int) = a + b  // getting values and immediate returning it with answer.
+
+fun sum2(
+    a: Int,
+    b: Int,
+    c: Int
+): Unit {   // unit is something which doesnt returns any value just like void in C# else if dont write "unit" it will still consider void.
+
+    println("Sum2 is ${a + b + c}")
+
+}
+
+fun maxOf(a: Int, b: Int) = if (a > b) a else b // expression conditions
+
+fun parseInt(str: String) = if (str.toInt() == str.toInt()) str.toInt() else if (str.toInt() !is Int) null else null
+
+
+fun printProductofnumer(arg1: String, arg2: String) {
+    var x = parseInt(arg1)
+    var y = parseInt(arg2)
+
+    if (x != null && y != null) {
+        println("Product is " + x * y);
+    } else {
+        println("$x or $y is not a number")
+    }
+
+
+}
+
+fun CheckStringLen(str: String): Int? {
+    if (str is String) {
+        return str.length
+    }
+    return null
+}
+
+fun loopCheck() {
+    val items = listOf<String>("Apple", "Banana", "Kiwi")
+    for (item in items) {
+        println("for Loop items " + item)
+    }
+}
+
+fun whileCheck() {
+    val items = listOf<String>("Apple", "Banana", "Kiwi")
+    var index = 0
+    while (index < items.size) {
+        println("while loop items " + items[index])
+        index++
+    }
+}
+
+fun DescribeObjTrype(obj: Any): String =
+    when (obj) {
+        1 -> "one"
+        "Hello" -> "Greeting"
+        is Long -> "Long"
+        !is String -> "Not a String"
+        else -> "Unknown Object type"
+    }
 
    // fun addition(a:Int,b:Int):Int{
        // return a+b;
--- app/src/main/java/com/vlxstudios/kotlinbasicss/main.kt
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/main.kt
@@ -1,10 +1,33 @@
 package com.vlxstudios.kotlinbasicss
 
-class vlx {
-public  fun add(a:Int,b:Int):Int{
-    return a+b;
-}
-}
-fun main(args: Array<String>){
-    println("Hello kotlin")
+class vlx (val firstname:String,val lastName:String){
+    var a:String
+    var b : String
+    var c :String
+    var d: String
+    init {
+        // this block usually used to initialize the values if we declare inside the class
+        //it is also known as a constructor/acts as a constructor
+        // a class can have more than init{} blocks
+        // now we initialize the values decalred inside the class
+        a=firstname
+        b= lastName
+
+        println("first init block")
+    }
+    init {
+        //this is second init in which
+        c = firstname
+        d= lastName
+        println("Second init block")
+    }
+
+    // or we can do write a constructor keyword like this
+
+    constructor(): this("Peter","Parker") {
+        println("Secondary Constructor")
+        println("$a $b   <- this is called from secondary constructor")
+        // this is refered to a secondary constructor
+        //this constructor is only processed after completing all init functions
+    }
 }
