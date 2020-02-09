355e28e9587de87248ad654cdb502dcc63416a9d
Vishal Lakhani <vishal4projects@outlook.com>
2020-02-09 12:53:51 +0530

    commit changes

--- app/src/main/java/com/vlxstudios/kotlinbasicss/EntityFactory.kt
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/EntityFactory.kt
@@ -14,8 +14,28 @@ class Entity private  constructor(val id:String){
         fun create() = Entity(getId()) // a companion is an object scoped to an instance within/another class
     }
 }
+//=================================================================
+class Entity2  constructor(val id:String){
+
+    companion object Factory {
+
+        fun create2() = Entity2("id")
+
+        }
+    }
+
+
+object  EntityFactory {
+    fun create() = Entity2("id")
+
+}
+//=========================================================================
+
 fun main(){
     val entity = Entity.Factory.create()
+    val entity2 = EntityFactory.create()
+    println(entity2 .toString());
+
     entity.id;
     println("$entity :Entity")
 }
