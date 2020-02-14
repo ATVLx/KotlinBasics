14f1b35c380f5891ee7e39359b7d1b13b9c34845
Vishal <vlxmenblack@gmail.com>
2020-02-14 18:24:32 +0530

    update working ticTacTow

--- app/src/main/java/com/vlxstudios/kotlinbasicss/TictactoeOOP.kt
+++ app/src/main/java/com/vlxstudios/kotlinbasicss/TictactoeOOP.kt
@@ -2,22 +2,21 @@ package com.vlxstudios.kotlinbasicss
 
 class GameMatrix {
 
+    var moves:Int =0
+    var gameOver : Boolean = false
+    var printed:Boolean= false
+    var mainPlayer : String = "X"
+    var board: Array<Array<String>> = arrayOf(
+         arrayOf("1", "2", "3"),
+         arrayOf("4", "5", "6"),
+        arrayOf("7","8","9")
+     )
+   
 
-  var board: Array<Array<String>> = arrayOf(
-      arrayOf("1", "2", "3"),
-      arrayOf("4", "5", "6"),
-      arrayOf("7","8","9")
-  )
 
-
-    var recivedValue: Int? = 0
-    var printed: Boolean = false
-
-
-
-    fun placeElements(row:Int,col:Int,value:Int){
-        board[row][col] = value.toString()
-    }
+    
+    fun isGameOver():Boolean = gameOver
+    private var receivedValue: Int? = 0
 
     fun displayMatrix() {
 
@@ -32,49 +31,113 @@ class GameMatrix {
         }
 
 
-    fun giveValue(number: Int, playerType:String) {
-        recivedValue = number
+    fun giveValue(number: Int) {
+        receivedValue = number
         println("REcived number is : $number")
-       checkValue(playerType)
+       checkValue(mainPlayer)
+
+        moves++
     }
 
-    fun checkValue(playerInitials:String) {
+    private fun checkValue(playerInitials:String) {
 
-        var row:Int = -1
-        var column:Int = -1
-        printed = false
-        for (i in board) {
-
+         var row:Int = -1
+         var column:Int = -1
+         printed = false
+         for (i in board) {
             row++
             for (j in i) {
                 column++
-                if (recivedValue == j.toInt()) {
-                    println(" Found")
 
-                   DebugfoundValue(row,column)
+
+                if (receivedValue.toString() == j) {
+                   //found
                     board[row][column] = playerInitials
                    displayMatrix()
+                    playerSwitcher()
+
                    return
 
                 } else {
+                    if(!printed){
+                        println("Please enter into Another space")
+                        printed = true
+                       // playerSwitcher()
+                    }
 
-                      //  println("  Not Found!")
-
-                }
+                     }
 
             }
-            column = -1
+            column = -1 //reset column when it completes
 
 
 
         }
 
 
+
+
+
+
+        }
+
+    fun playerSwitcher() = when (mainPlayer) {
+        "X" -> mainPlayer = "O"
+        "O" -> mainPlayer = "X"
+        else -> mainPlayer = "X"
     }
 
-    fun DebugfoundValue( rowVal: Int,colVal:Int){
-        println("Found Values row: $rowVal and col: $colVal")
+     fun checkWinner( ){
+
+        println("Moves $moves")
+        if( moves >=3) {
+            if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") {
+                print(" Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+            } else// upperDeck
+                if (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") {
+                    print(" Player win") ; gameOver=true;// gameMgr.gameOver=gameOver
+                } else // middle Deck
+                    if (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") {
+                        print(" Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                    } else// lower Deck
+                        if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") {
+                            print(" Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                        } else// Left slash Deck
+                            if (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") {
+                                print(" Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                            } else// center vertical deck
+                                if (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") {
+                                    print(" Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                                } // Right Slash Deck
+                                else// center vertical deck
+                                    if (board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") {
+                                        print(" Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                                    }
+        }
+
+        if( moves >=3) {
+            if (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") {
+                print("O Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+            } else// upperDeck
+                if (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O") {
+                    print(" O Player win") ; gameOver=true;// gameMgr.gameOver=gameOver
+                } else // middle Deck
+                    if (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") {
+                        print("O Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                    } else// lower Deck
+                        if (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
+                            print(" O Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                        } else// Left slash Deck
+                            if (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
+                                print(" O Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                            } else// center vertical deck
+                                if (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O") {
+                                    print(" O Player win") ; gameOver=true ; //gameMgr.gameOver=gameOver
+                                } // Right Slash Deck
+        }
     }
+
+
 
 }
  //class end
@@ -82,38 +145,42 @@ class GameMatrix {
 fun main() {
 
 
-var maingame:MainGame = MainGame()
+    var maingame:MainGame = MainGame()
     maingame.initializeGame()
     maingame.runGame()
 
 
     var userInputProvider: UserInput = UserInput()
-    userInputProvider.giveValuetoMatrix()
+    userInputProvider.giveValueToMatrix()
 
 
 }
 
 open class UserInput { // class remains closed by Default so we need to add open before class inorder to inherit
 
-    private var userInput:Int = 0
-    var playerName:String =""
+    private var userInput:Int
+
+
+    init {
+
+        userInput = 0
+    }
     var matrix: GameMatrix = GameMatrix()
-        fun getUserInput(inputNumber:Int, player:String) {
+        fun getUserInput(inputNumber:Int) {
 
             userInput = inputNumber
-            playerName = player;
+
 
          }
 
 
-         fun giveValuetoMatrix() {
-             matrix.giveValue(userInput.toInt(),playerName)
-
+         fun giveValueToMatrix() {
+             matrix.giveValue(userInput.toInt())
         }
+    
+        
 
-            fun applyInputToMatrix(){
-               // matrix.checkValue()
-            }
+
 
 
 
@@ -124,8 +191,11 @@ class  MainGame  : UserInput(){
 
     var gameOver : Boolean = false
     var game: GameMatrix = GameMatrix()
-    var mainPlayer : String = "X"
+    var playerMain = game.mainPlayer
+
     var value:Int = 0
+    //var winPlayer = ""
+    
 
 
     fun initializeGame(){
@@ -136,27 +206,33 @@ class  MainGame  : UserInput(){
 
 
 
-    fun playerSwitcher() = when (mainPlayer) {
-        "X" -> mainPlayer = "O"
-        "O" -> mainPlayer="X"
-        else -> mainPlayer="X"
 
 
 
-    }
+
 
     fun runGame() {
 
         gameOver = false
         while(!gameOver){
-            println("Player: $mainPlayer's Turn please Enter the valid number from given Number Matrix ")
-            getUserInput(readLine()!!.toInt(),mainPlayer)
-            playerSwitcher()
-            giveValuetoMatrix()
-            //applyInputToMatrix()
+
+                playerMain = game.mainPlayer
+                println("Player: $playerMain's Turn please Enter the valid number from given Number Matrix ")
+                getUserInput(readLine()!!.toInt())
+                game.playerSwitcher()
+                giveValueToMatrix()
+                matrix.checkWinner()
+                gameOver = matrix.isGameOver()
+
+
 
 
 
         }
+
     }
 }
+
+
+
+
